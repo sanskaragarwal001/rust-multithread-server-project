a97046404b6944617a96c0b113fab6cdae6bca78
Sanskar Agarwal <aggrawalsanskar@gmail.com>
2026-06-11 21:45:52 +0530

    update the fn handle_connection code where fn return the html file response according to the request

--- src/main.rs
+++ src/main.rs
@@ -20,11 +20,21 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("index.html").unwrap();
-    let length = contents.len();
+    if let Some(request_line) = http_request.get(0) {
+        if request_line == "GET / HTTP/1.1" {
+            let status_line = "HTTP/1.1 200 OK";
+            let contents = fs::read_to_string("index.html").unwrap();
+            let length = contents.len();
 
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+            let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+            stream.write_all(response.as_bytes()).unwrap();
+        } else {
+            let status_line = "HTTP/1.1 404 NOT FOUND";
+            let contents = fs::read_to_string("404.html").unwrap();
+            let length = contents.len();
 
-    stream.write_all(response.as_bytes()).unwrap();
+            let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+            stream.write_all(response.as_bytes()).unwrap();
+        }
+    }
 }
